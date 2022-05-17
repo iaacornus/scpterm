1ea8e3add2de1d8517d150c22e050717058e6344
iaacornus <iaacornus.devel@gmail.com>
2022-05-18 01:13:34 +0800

    some colors for output not supported by rich

---
+++ src/misc/colors.py
@@ -0,0 +1,27 @@
+class Colors:
+    END      = "\33[0m"
+    BOLD     = "\33[1m"
+    ITALIC   = "\33[3m"
+    URL      = "\33[4m"
+    BLINK    = "\33[5m"
+    BLINK2   = "\33[6m"
+    SELECTED = "\33[7m"
+
+    BLACK  = "\33[30m"
+    RED    = "\33[31m"
+    GREEN  = "\33[32m"
+    YELLOW = "\33[33m"
+    BLUE   = "\33[34m"
+    VIOLET = "\33[35m"
+
+    GREY    = "\33[90m"
+    RED2    = "\33[91m"
+    GREEN2  = "\33[92m"
+    YELLOW2 = "\33[93m"
+    BLUE2   = "\33[94m"
+    VIOLET2 = "\33[95m"
+
+    BRED = "\33[1;31m"
+    BBRED = "\33[1;91m"
+    BGREEN = "\33[1;32m"
+    BBGREEN = "\33[1;92m
