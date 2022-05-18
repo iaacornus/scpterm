c54913469684acf76f66407444f028b1cc34aa63
iaacornus <iaacornus.devel@gmail.com>
2022-05-18 11:45:46 +0800

    create anomalies.list.d dir if not exist

--- src/utils/database_init.py
+++ src/utils/database_init.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append("..")
 
+import os
 from datetime import datetime as time
-from os import path
 
 import requests
 from random_user_agent.user_agent import UserAgent
@@ -14,12 +14,12 @@ def database_init():
     user_agent = UserAgent()
     link = "https://the-scp.foundation/object"
 
-    if path.isfile("database/fetch"):
+    if os.path.isfile("database/fetch"):
         with open("database/fetch", "r") as data:
             data_info = data.readlines()
 
         console.log(
-            f"[bold][+] Database already exists (last fetch:[cyan]{data_info[1]}[/cyan]), skipping.[/bold]"
+            f"[bold][+] Database already exists (last fetch:[cyan]{data_info[1]}[/cyan]).[/bold]"
         )
     else:
         try:
@@ -28,7 +28,7 @@ def database_init():
                 spinner="bouncingBar"
             ):
                 header = {"User-Agent": user_agent.get_random_user_agent()}
-                #response = requests.get(link, headers=header)
+                response = requests.get(link, headers=header)
         except ConnectionError:
             console.log(
                 "[bold red][-] Database is offline, cannot initiate[/bold red]"
@@ -41,6 +41,12 @@ def database_init():
 
             with open("database/fetch" ,"w") as data:
                 data.write(f"Fetched data:\n{time.now().strftime('%d:%m/%H:%M:%S')}")
+
+            if not os.path.exists(f"database/anomalies.list.d"):
+                console.log(
+                    "[turquoise4]> Creating directory for anomalies data ...[/turquoise4]"
+                )
+                os.mkdir(f"database/anomalies.list.d")
 
             with console.status(
                 "[bold turquoise4][=] Fetching anomalies information ...[/bold turquoise4]",
@@ -58,7 +64,7 @@ def database_init():
                         anomalies.write(f"SCP-{scp_num}: {scp_link}\n")
 
                         console.log(
-                            f"[turquoise4]> Fetching [turquoise4][bold wheat4]SCP-{scp_num}[/bold wheat4]"
+                            f"[turquoise4]> Fetching [turquoise4][cyan]SCP-{scp_num}[/cyan]"
                         )
 
                     # fetch the html from the page
